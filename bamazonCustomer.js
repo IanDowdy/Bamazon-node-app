07388c5652b02c0bfacabd36113b30272b1393c7
Ian Dowdy <ian_dowdy@outlook.com>
2018-10-23 17:16:47 -0500

    small update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,12 +15,24 @@ function intro() {
     console.log(chalk.bgCyan("WELCOME! Today we have some great products for you!"))
     con.query(
         "SELECT * FROM products",
-        function(err, res) {
-            for ( i = 0; i < res.length; i ++) {
+        function (err, res) {
+            for (i = 0; i < res.length; i++) {
                 console.log(res[i].product_name + " from the " + res[i].department_name + " department at a price of $" + res[i].price + " each.");
             }
         }
     );
+    inquirer.prompt({
+        name: "intro",
+        type: "confirm",
+        message: "...Ohai, care to do some shopping?",
+        default: true
+    }).then(function (ans) {
+        if (ans) {
+            console.log(ans);
+        } else {
+            console.log("Failed");
+        }
+    })
 }
 
 intro();
